05b86302f16d282899c3689ccf1258d7767c2cdd
Pedro <pedro.henrique.pereira.2014@gmail.com>
2025-11-09 18:54:38 -0300

    Removido as rotas desnecessarias no componente app

--- arcane-stats/src/App.tsx
+++ arcane-stats/src/App.tsx
@@ -1,29 +1,17 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Header from './pages/Header'
 import Sidebar from './pages/Sidebar'
-import Dashboard from './pages/Dashboard'
-import Times from './pages/Times'
-import Jogadores from './pages/Jogadores'
-import Partidas from './pages/Partidas'
-import Configuracoes from './pages/Configuracoes'
+import AnimatedRoutes from './pages/AnimatedRoutes'
 
 function App() {
-
   return (
     <BrowserRouter>
       <div className='flex min-h-screen'>
         <Sidebar />
         <div className='flex-1 flex flex-col'>
           <Header />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/times" element={<Times />} />
-            <Route path="/jogadores" element={<Jogadores />} />
-            <Route path="/partidas" element={<Partidas />} />
-            <Route path="/configuracoes" element={<Configuracoes />} />
-          </Routes>
+          <AnimatedRoutes />
         </div>
       </div>
     </BrowserRouter>
