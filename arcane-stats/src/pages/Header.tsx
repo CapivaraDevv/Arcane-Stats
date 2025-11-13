41db28d75d6f56154f55358ae2ccfd119739d1e1
PedroHenrique <pedro.henrique.pereira.2014@gmail.com>
2025-11-13 00:04:07 -0300

    Pequenas mudanças no visual

--- arcane-stats/src/pages/Header.tsx
+++ arcane-stats/src/pages/Header.tsx
@@ -3,9 +3,11 @@ const Header = () => {
         <>
             <header className="w-full px-8 py-6 bg-[#1D2D50] border-b border-white/5 flex items-center justify-between shadow-lg">
                 <h1 className="space-grotesk-title text-2xl font-semibold text-[#E0E0E0]">Bem-vindo à Plataforma de Análise eSports</h1>
-                <img src="/LogoBranco.png" alt="Logo" className="w-20" />
+                <div className="w-40 bg-white flex items-center justify-center rounded-3xl">
+                    <img src="/LogoBranco.png" alt="Logo" className="w-20" />
+                </div>
             </header>
-        </> 
+        </>
     )
 }
 
